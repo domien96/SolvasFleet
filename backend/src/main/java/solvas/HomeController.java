0d6d6bed18d84453900bd404a4a9fa525144e70d
David Vandorpe <david.vandorpe@ugent.be>
2017-03-03 01:21:28 +0100

    Removed ManageCompany class

--- backend/src/main/java/solvas/HomeController.java
+++ backend/src/main/java/solvas/HomeController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import solvas.database.Dao;
 import solvas.models.Company;
-import solvas.models.ManageCompany;
 
 @RestController
 public class HomeController {
