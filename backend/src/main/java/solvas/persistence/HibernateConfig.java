13149407167ef5923b14c4f0d7467f3cf7180778
David Vandorpe <david.vandorpe@ugent.be>
2017-03-03 23:55:30 +0100

    Fix hibernate when running from a jar
    Taken from
    http://stackoverflow.com/questions/33437480/spring-boot-hbm-xml-mapping-files-not-loaded-in-test

--- backend/src/main/java/solvas/persistence/HibernateConfig.java
+++ backend/src/main/java/solvas/persistence/HibernateConfig.java
@@ -2,18 +2,20 @@ package solvas.persistence;
 
 import org.hibernate.cfg.AvailableSettings;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.ResourcePatternUtils;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.util.Properties;
 
 /**
@@ -41,21 +43,35 @@ public class HibernateConfig {
         return dataSource;
     }
 
+
+    @Autowired
+    private ResourceLoader rl;
+
+    private Resource[] loadResources() {
+        Resource[] resources = null;
+        try {
+            resources = ResourcePatternUtils.getResourcePatternResolver(rl)
+                    .getResources("classpath:/mappings/*.hbm.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resources;
+    }
+
+
+
     /**
      * The session factory bean. Spring manages everything for us.
-     *
-     * @param context Injected application context.
      *
      * @return The factory.
      */
     @Bean
-    public LocalSessionFactoryBean sessionFactory(ApplicationContext context) {
+    public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(getDataSource());
         sessionFactory.setHibernateProperties(getHibernateProperties());
         // Automatically load mappings.
-        Resource resource = context.getResource("classpath:mappings/");
-        sessionFactory.setMappingDirectoryLocations(resource);
+        sessionFactory.setMappingLocations(loadResources());
         return sessionFactory;
     }
 
