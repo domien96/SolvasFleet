30881901183e4f32034cb10655c948e70d8e4f16
Niko Strijbol <strijbol.niko@gmail.com>
2017-03-05 18:09:04 +0100

    Remove unused variable

--- backend/src/main/java/solvas/HomeController.java
+++ backend/src/main/java/solvas/HomeController.java
@@ -1,10 +1,8 @@
 package solvas;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import solvas.models.Company;
-import solvas.persistence.company.CompanyDao;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -15,17 +13,9 @@ import java.util.Collection;
 @RestController
 public class HomeController {
 
-    private final CompanyDao dao;
-
-    @Autowired
-    public HomeController(CompanyDao dao) {
-        this.dao = dao;
-    }
-
     @RequestMapping("/")
     public Collection<Company> index() {
         //TODO make controller for root if necessary
         return new ArrayList<>();
     }
- 
 }
