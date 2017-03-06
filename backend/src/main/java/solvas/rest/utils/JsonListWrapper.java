3077986c8519c662e9ae8d65c6664deccd5541e1
David Vandorpe <david.vandorpe@ugent.be>
2017-03-06 19:29:14 +0100

    Document JsonListWrapper

--- backend/src/main/java/solvas/rest/utils/JsonListWrapper.java
+++ backend/src/main/java/solvas/rest/utils/JsonListWrapper.java
@@ -4,15 +4,27 @@ import java.util.Collection;
 import java.util.HashMap;
 
 /**
- * Created by David Vandorpe.
+ * Class to wrap JSON arrays in a JSON object
  */
 public class JsonListWrapper<T> extends HashMap<String, Collection<T>> {
+    /*
+     * Default no-arg constructor. You can set keys as you would for a map
+     */
     public JsonListWrapper() {}
 
+    /*
+     *  Use the default key ("items")
+     *
+     *  @param items the collection that should be wrapped
+     */
     public JsonListWrapper(Collection<T> items) {
         this(items, "items");
     }
 
+    /*
+     * @param items the collection that should be wrapped
+     * @paaram key name of the key that should contain the items
+     */
     public JsonListWrapper(Collection<T> items, String key) {
         put(key, items);
     }
