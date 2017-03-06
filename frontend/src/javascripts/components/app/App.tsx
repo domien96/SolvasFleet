0633e181bae3650c2d6f6e49b00bf66294e86419
benji <benj_cous@hotmail.com>
2017-03-06 13:14:31 +0100

    Add some styling

--- frontend/src/javascripts/components/app/App.tsx
+++ frontend/src/javascripts/components/app/App.tsx
@@ -1,20 +1,31 @@
 import React    from 'react';
 import { Link } from 'react-router';
 
-import Header from './Header.tsx';
+class Header extends React.Component<{}, {}> {
+  render() {
+    return (
+      <nav className='navbar-default navbar-side'>
+        <ul className='nav'>
+          <li>
+            <Link to='/' >Home</Link>
+          </li>
+          <li className='active'>
+            <Link to='/companies' >Companies</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 class App extends React.Component<{}, {}> {
   render() {
     return (
       <div id='wrapper'>
         <Header />
-        <div>Dit is Solvas Fleet</div>
-        <div>
-          <Link to='/'>Root</Link>
-          <Link to='/companies'>Companies</Link>
-          <Link to='/blargh'>Dit bestaat niet</Link>
+        <div className='page-wrapper'>
+          { this.props.children }
         </div>
-        { this.props.children }
       </div>
     );
   }
