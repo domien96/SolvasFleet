93f1fc42a9771f887b46f43398470d0683aa5318
David Vandorpe <david.vandorpe@ugent.be>
2017-03-07 19:35:40 +0100

    Begin of gamification setup

---
+++ ansible/roles/gamification/templates/config/initializers/github.rb
@@ -0,0 +1,8 @@
+# Github accessor
+Rails.configuration.x.github = Github.new(
+    endpoint: 'https://github.ugent.be/api/v3',
+    site: 'https://github.ugent.be',
+    upload_endpoint: 'https://github.ugent.be/api/uploads',
+    oauth_token: Rails.application.secrets.github_token,
+    auto_pagination: true
+)
