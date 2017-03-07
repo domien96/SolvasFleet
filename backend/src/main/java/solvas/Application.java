e50cee99227e6c6544f86c94d33703f338f6b336
David Vandorpe <david.vandorpe@ugent.be>
2017-03-07 14:57:10 +0100

    Document Application class

--- backend/src/main/java/solvas/Application.java
+++ backend/src/main/java/solvas/Application.java
@@ -8,12 +8,19 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
- 
+
+/**
+ * The SolvasFleet application bootstrap
+ */
 @Configuration
 @EnableAutoConfiguration(exclude = {HibernateJpaAutoConfiguration.class})
 @ComponentScan(value = {"solvas"})
 public class Application {
- 
+
+    /**
+     * Start the SolvasFleet application
+     * @param args The args passed in the command line
+     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
