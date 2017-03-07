0775f30caa4f2017364ed9ca5670dd0b36d10d68
Niko Strijbol <strijbol.niko@gmail.com>
2017-03-07 22:25:04 +0100

    Remove test home controller (pushed by accident)

--- backend/src/main/java/solvas/HomeController.java
+++ backend/src/main/java/solvas/HomeController.java
@@ -1,10 +1,8 @@
 package solvas;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import solvas.models.Company;
-import solvas.persistence.company.CompanyDao;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -15,27 +13,9 @@ import java.util.Collections;
 @RestController
 public class HomeController {
 
-    private final CompanyDao dao;
-
-    @Autowired
-    public HomeController(CompanyDao dao) {
-        this.dao = dao;
-    }
-
     @RequestMapping("/")
     public Collection<Company> index() {
         //TODO make controller for root if necessary
-
-        Company company = new Company();
-        company.setName("test");
-        company.setAddress("lol");
-        dao.save(company);
-//        Company company = dao.find(20);
-//        company.setName("Test 22222222");
-//        company.setAddress("lololol");
-
-       // dao.save(company);
-
-        return Collections.singleton(company);
+        return Collections.emptyList();
     }
 }
