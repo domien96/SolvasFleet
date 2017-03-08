38c126612ffdd55860eb054c7b05a87fc80ca95f
klivdnbu <karel.vandenbussche@ugent.be>
2017-03-08 22:23:51 +0100

    Revert "small test"
    This reverts commit 25c85c4e53d2e70f8b8cf321fe923b99c346fabb.

--- backend/src/itest/java/dao/CompanyDaoTest.java
+++ backend/src/itest/java/dao/CompanyDaoTest.java
@@ -2,46 +2,24 @@ package dao;
 
 import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.context.annotation.Profile;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import solvas.models.Company;
-import solvas.persistence.HibernateConfig;
 import solvas.persistence.company.CompanyDao;
 import solvas.persistence.company.HibernateCompanyDao;
 
-import javax.sql.DataSource;
 import javax.transaction.Transactional;
 
 import static org.junit.Assert.assertEquals;
 
-
+@Ignore
 @Transactional
-@RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles("test")
-@ContextConfiguration(classes = HibernateConfig.class,loader = AnnotationConfigContextLoader.class)
 public class CompanyDaoTest {
 
-
-    @Autowired
-    private CompanyDao companyDao;
-
-    @Autowired
-    private DataSource dataSource;
+    private CompanyDao companyDao=new HibernateCompanyDao();
 
     @Test
     public void addCompany()
     {
-        Company company = new Company("test","oo","99","OOO","aa");
-        company.setId(0);
-        companyDao.save(company);
+
     }
 
     @Test
