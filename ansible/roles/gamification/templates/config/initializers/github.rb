7ad76e18020b029d880760e645c34e722ee35c8b
David Vandorpe <david.vandorpe@ugent.be>
2017-03-11 21:33:49 +0100

    Don't put gamification in this PR
    Not finished yet

--- ansible/roles/gamification/templates/config/initializers/github.rb
+++
@@ -1,9 +0,0 @@
-# Github accessor
-Rails.configuration.x.github = Github.new(
-    endpoint: 'https://github.ugent.be/api/v3',
-    site: 'https://github.ugent.be',
-    upload_endpoint: 'https://github.ugent.be/api/uploads',
-    oauth_token: Rails.application.secrets.github_token,
-    auto_pagination: true
-)
-
