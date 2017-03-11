d0a7a103c7c994b5ff4b2809d59e4dc090574329
David Vandorpe <david.vandorpe@ugent.be>
2017-03-11 21:27:20 +0100

    Fix symlink issues with webpack

--- config/deploy/frontend.rb
+++ config/deploy/frontend.rb
@@ -13,6 +13,7 @@ set :repo_tree, 'frontend'
 after 'deploy:updated', 'npm:build'
 
 set :linked_files, %w(webpack.config.js src/javascripts/constants/constants.ts)
+set :linked_dirs, %w(node_modules)
 
 
 namespace :npm do
@@ -32,3 +33,25 @@ namespace :npm do
     end
   end
 end
+
+namespace :deploy do
+  namespace :symlink do
+  Rake::Task['linked_files'].clear_actions
+    desc "Symlinks break webpack, use cp"
+    task :linked_files do
+      next unless any? :linked_files
+      on release_roles :all do
+        execute :mkdir, "-p", linked_file_dirs(release_path)
+
+        fetch(:linked_files).each do |file|
+          target = release_path.join(file)
+          source = shared_path.join(file)
+          next if test "[ -L #{target} ]"
+          execute :rm, target if test "[ -f #{target} ]"
+          execute :cp, source, target
+        end
+      end
+    end
+  end
+end
+
