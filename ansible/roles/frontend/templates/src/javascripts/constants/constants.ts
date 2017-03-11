d0cad90a69e61768359eb89533626b223489a5a0
David Vandorpe <david.vandorpe@ugent.be>
2017-03-11 01:12:42 +0100

    Install node

---
+++ ansible/roles/frontend/templates/src/javascripts/constants/constants.ts
@@ -0,0 +1,5 @@
+export const INITIAL_LIKES:number = 0;
+
+export const COMPANIES_URL = https://vopro6.ugent.be/rest/companies';
+export const USERS_URL = 'https://vopro6.ugent.be/rest/users';
+export const VEHICLES_URL = 'https://vopro6.ugent.be/rest/vehicles';
