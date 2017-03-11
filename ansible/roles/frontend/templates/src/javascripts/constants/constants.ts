9866e195d392aed32046fda351833032cd2f43fa
David Vandorpe <david.vandorpe@ugent.be>
2017-03-11 18:28:31 +0100

    Fixed missing quote

--- ansible/roles/frontend/templates/src/javascripts/constants/constants.ts
+++ ansible/roles/frontend/templates/src/javascripts/constants/constants.ts
@@ -1,5 +1,5 @@
 export const INITIAL_LIKES:number = 0;
 
-export const COMPANIES_URL = https://vopro6.ugent.be/rest/companies';
+export const COMPANIES_URL = 'https://vopro6.ugent.be/rest/companies';
 export const USERS_URL = 'https://vopro6.ugent.be/rest/users';
 export const VEHICLES_URL = 'https://vopro6.ugent.be/rest/vehicles';
