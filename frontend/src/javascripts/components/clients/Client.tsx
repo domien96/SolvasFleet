54469611da6fe651c735e0a006b7b392d1529a1f
nnmak <nils.mak@gmail.com>
2017-03-12 19:49:01 +0100

    added EditClient Component

--- frontend/src/javascripts/components/clients/Client.tsx
+++ frontend/src/javascripts/components/clients/Client.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { browserHistory } from'react-router';
+import { browserHistory, Link } from'react-router';
 
 import fetchClient from '../../actions/fetch_company.ts';
 import deleteClient from '../../actions/delete_company.ts';
@@ -55,10 +55,9 @@ class Client extends React.Component<Company.Props, Company.State> {
                 <div className='col-sm-4'>
                   <div className='row actions'>
                     <div className='col-sm-6'>
-                      <button className='btn btn-default form-control'>
-                        <span className='glyphicon glyphicon-edit' />
-                        Edit
-                      </button>
+                      <Link to={ '/clients/' + id + '/edit' } className='btn btn-default form-control'>
+                        <span className='glyphicon glyphicon-edit' /> Edit
+                      </Link>
                     </div>
                     <div className='col-sm-6'>
                       <button onClick = { this.deleteClient } className='btn btn-danger form-control'>
