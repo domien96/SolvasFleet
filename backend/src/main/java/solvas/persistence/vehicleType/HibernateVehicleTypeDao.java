3014ebb4cf90f4dad8b75403601dfa308e6e3bc6
Niko Strijbol <strijbol.niko@gmail.com>
2017-03-12 19:54:55 +0100

    Wrong filter

--- backend/src/main/java/solvas/persistence/vehicleType/HibernateVehicleTypeDao.java
+++ backend/src/main/java/solvas/persistence/vehicleType/HibernateVehicleTypeDao.java
@@ -2,12 +2,10 @@ package solvas.persistence.vehicleType;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import solvas.models.Vehicle;
 import solvas.models.VehicleType;
 import solvas.persistence.Filter;
 import solvas.persistence.HibernateDao;
 
-import javax.persistence.criteria.Join;
 import java.util.Collection;
 
 /**
@@ -28,12 +26,8 @@ public class HibernateVehicleTypeDao extends HibernateDao<VehicleType> implement
 
     @Override
     public Collection<VehicleType> withType(String name) {
-        return findAll(Filter.predicate((builder, root) -> {
-            Join<Vehicle, VehicleType> join = root.join("type");
-            return builder.equal(
-                    join.get("name"),
-                    name
-            );
-        }));
+        return findAll(Filter.predicate((builder, root) -> builder.equal(
+                root.get("name"), name
+        )));
     }
 }
