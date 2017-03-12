456835222631effc7c712d36f4b215cfd59bd8e7
sjabasti <Steven.Bastiaens@UGent.be>
2017-03-12 17:58:14 +0100

    homecontroller removed example

--- backend/src/main/java/solvas/HomeController.java
+++ backend/src/main/java/solvas/HomeController.java
@@ -1,38 +1,23 @@
 package solvas;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import solvas.models.Company;
-import solvas.persistence.company.CompanyDao;
 
 import java.util.Collection;
 import java.util.Collections;
 
 /**
  * Unfinished root controller, check if necessary
+ * TODO make controller for root if necessary
  */
 @RestController
 public class HomeController {
 
-    @Autowired
-    CompanyDao companyDao;
-
     @RequestMapping("/")
-    public Collection<Company> index() {
-        Company company = new Company();
-        company.setName("Name");
-        company.setVatNumber("Vat");
-        company.setPhoneNumber("phone");
-        company.setAddressCity("city");
-        company.setAddressCountry("country");
-        company.setAddressHouseNumber("housenumber");
-        company.setAddressPostalCode("postalcode");
-        company.setAddressStreet("street");
-        companyDao.save(company);
+    public String index() {
 
-
-        //TODO make controller for root if necessary
-        return Collections.emptyList();
+        return "Vop6";
     }
 }
