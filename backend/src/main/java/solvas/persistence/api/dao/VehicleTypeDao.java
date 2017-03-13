d33183e8d38e7b7c861a2829f6a8a3f2d3e59543
Niko Strijbol <strijbol.niko@gmail.com>
2017-03-13 21:48:21 +0100

    Sonarqube III

--- backend/src/main/java/solvas/persistence/api/dao/VehicleTypeDao.java
+++ backend/src/main/java/solvas/persistence/api/dao/VehicleTypeDao.java
@@ -9,5 +9,17 @@ import java.util.Collection;
  * Dao for vehicleTypes
  */
 public interface VehicleTypeDao extends Dao<VehicleType> {
+
+    /**
+     * Get all models with a given type.
+     *
+     * TODO: we should implement constraints in the database to prevent two
+     * models with the same type, so an uniqueness constraint should be applied.
+     * Once that is done, this method can return one type instead of a collection.
+     *
+     * @param type The name of the type.
+     *
+     * @return Should be a collection with one type.
+     */
     Collection<VehicleType> withType(String type);
 }
