49f4076c6144e4ac44b0264f786ace61082ee977
klivdnbu <karel.vandenbussche@ugent.be>
2017-03-14 12:39:08 +0100

    add dao's to testconfig

--- backend/src/itest/java/dao/HibernateTestConfig.java
+++ backend/src/itest/java/dao/HibernateTestConfig.java
@@ -1,65 +1,28 @@
 package dao;
 
-import org.hibernate.cfg.AvailableSettings;
-import org.springframework.beans.factory.BeanCreationException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.io.support.ResourcePatternUtils;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Properties;
-
-import org.hibernate.cfg.AvailableSettings;
-import org.springframework.beans.factory.BeanCreationException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.core.io.support.ResourcePatternUtils;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import solvas.models.Vehicle;
 import solvas.persistence.company.CompanyDao;
 import solvas.persistence.company.HibernateCompanyDao;
+import solvas.persistence.fleet.FleetDao;
+import solvas.persistence.fleet.HibernateFleetDao;
+import solvas.persistence.fleetSubscription.FleetSubscriptionDao;
+import solvas.persistence.fleetSubscription.HibernateFleetSubscriptionDao;
 import solvas.persistence.role.HibernateRoleDao;
 import solvas.persistence.role.RoleDao;
+import solvas.persistence.subFleet.HibernateSubFleetDao;
+import solvas.persistence.subFleet.SubFleetDao;
 import solvas.persistence.user.HibernateUserDao;
 import solvas.persistence.user.UserDao;
 import solvas.persistence.vehicle.HibernateVehicleDao;
 import solvas.persistence.vehicle.VehicleDao;
 
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.util.Properties;
 
 /**
- * Hibernate configuration class.
- *
- * Provides various Spring beans to use.
- *
- * @author Niko Strijbol
+ * Config for testing
  */
 @Configuration
 public class HibernateTestConfig {
@@ -69,16 +32,6 @@ public class HibernateTestConfig {
     public DataSource getDataSource() {
         return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
                 .addScript("db/migration/V1__milestone1.sql").addScript("db/migration/V2_1__n-m_relations.sql").addScript("schema.sql").build();
-/*
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:sample;INIT=RUNSCRIPT FROM 'classpath:schema.sql'");
-        //dataSource.setUrl("jdbc:h2:~/db/database.db;DB_CLOSE_ON_EXIT=TRUE;INIT=create schema IF NOT EXISTS generic;");
-        dataSource.setUsername("vop");
-        dataSource.setPassword("vop");
-
-
-        return dataSource;*/
     }
 
 
@@ -108,6 +61,15 @@ public class HibernateTestConfig {
     {
         return new HibernateVehicleDao();
     }
+
+    @Bean
+    public FleetDao fleetDao() { return new HibernateFleetDao();}
+
+    @Bean
+    public SubFleetDao subFleetDao() { return new HibernateSubFleetDao(); }
+
+    @Bean
+    public FleetSubscriptionDao fleetSubscriptionDao() { return new HibernateFleetSubscriptionDao(); }
 
 }
 
