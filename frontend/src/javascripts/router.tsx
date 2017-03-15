3d3e786a60a15b01837a58f18a025864c7cc0140
benji <benj_cous@hotmail.com>
2017-03-15 14:36:57 +0100

    Use SUB_URI on server

--- frontend/src/javascripts/router.tsx
+++ frontend/src/javascripts/router.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { useBasename } from 'history';
 
 import App       from './components/app/App.tsx';
 
@@ -26,32 +27,46 @@ import Fleets   from './components/fleets/Fleets.tsx';
 
 import NoMatch   from './components/NoMatch.tsx';
 
+declare var ENVIRONMENT: string;
+declare var SUB_URI: string;
+
+if (ENVIRONMENT == "development") {
+  var SUB_URI = "";
+} else {
+  var SUB_URI = "/app";
+}
+
 class SolvasRouter extends React.Component<{}, {}> {
   render() {
+    const history = useBasename(() => browserHistory)({
+      basename: SUB_URI
+    })
     return (
-      <Router history={ browserHistory } >
+      <Router history={ history } >
         <Route path="/" component={ App } >
 
           <IndexRoute component={ Home } />
-          <Route path="/users/new"      component={ AddUser  } />
-          <Route path="/users/:id/edit" component={ EditUser } />
-          <Route path="/users"          component={ Users    } >
+          <Route path="users/new"      component={ AddUser  } />
+          <Route path="users/:id/edit" component={ EditUser } />
+          <Route path="users"          component={ Users    } >
             <IndexRoute component={ NoUser } />
-            <Route path="/users/:id" component={ User } />
+            <Route path="users/:id" component={ User } />
           </Route>
 
-          <Route path="/clients"                component={ Clients    } />
-          <Route path="/clients/new"            component={ AddClient  } />
-          <Route path="/clients/:id"            component={ Client     } />
-          <Route path="/clients/:id/edit"       component={ EditClient } />
-          <Route path="/clients/:id/fleets"     component={ Fleets     } />
+          <Route path="clients"                component={ Clients    } />
+          <Route path="clients/new"            component={ AddClient  } />
+          <Route path="clients/:id"            component={ Client     } />
+          <Route path="clients/:id/edit"       component={ EditClient } />
+          <Route path="clients/:id/fleets"     component={ Fleets     } />
 
-          <Route path="/vehicles/new"      component={ AddVehicle  } />
-          <Route path="/vehicles/:id/edit" component={ EditVehicle } />
-          <Route path="/vehicles"          component={ Vehicles    }>
+          <Route path="vehicles/new"      component={ AddVehicle  } />
+          <Route path="vehicles/:id/edit" component={ EditVehicle } />
+          <Route path="vehicles"          component={ Vehicles    }>
             <IndexRoute component={ NoVehicle } />
-            <Route path="/vehicles/:id" component={ Vehicle } />
+            <Route path="vehicles/:id" component={ Vehicle } />
           </Route>
+
+          <Route path="fleets/:id" component={ Fleet } />
 
           <Route path="*" component={ NoMatch } />
         </Route>
