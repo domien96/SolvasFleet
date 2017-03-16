a2329f52010db2859c71d138c8f7fab1e42a991a
klivdnbu <karel.vandenbussche@ugent.be>
2017-03-16 15:13:28 +0100

    empty filtertest

--- backend/src/test/java/filters/CompanyFilterTest.java
+++ backend/src/test/java/filters/CompanyFilterTest.java
@@ -1,14 +1,6 @@
 package filters;
 
-import solvas.rest.query.CompanyFilter;
 
 public class CompanyFilterTest {
 
-    private CompanyFilter companyFilter;
-
-
-    public void simpleTest()
-    {
-        companyFilter=new CompanyFilter();
-    }
 }
