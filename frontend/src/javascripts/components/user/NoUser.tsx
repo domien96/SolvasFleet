48491e60ff680ceadd6729b92033c2b7b7752069
benji <benj_cous@hotmail.com>
2017-03-21 21:52:32 +0100

    Convert noUser to dumb component

--- frontend/src/javascripts/components/user/NoUser.tsx
+++ frontend/src/javascripts/components/user/NoUser.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class NoUser extends React.Component<{}, {}> {
-  render() {
-    return (
-      <div className='card-content'>
-        <h2>No User</h2>
-      </div>
-    );
-  }
+const NoUser : React.StatelessComponent<{}> = () => {
+  return (
+    <div className='card-content'>
+      <h2>No User</h2>
+    </div>
+  );
 }
 
 export default NoUser;
