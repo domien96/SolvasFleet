a4a57472c5b3a46d708e4aff935b40ad8ae5826d
Domien Van Steendam <domien.vansteendam@ugent.be>
2017-03-25 19:50:52 +0100

    Fixes issue 234

--- backend/src/main/resources/db/migration/V1_4__uniqueness_Vin.sql
+++ backend/src/main/resources/db/migration/V1_4__uniqueness_Vin.sql
@@ -1,1 +1,6 @@
 ALTER TABLE vehicles ADD UNIQUE (chassis_number);
+ALTER TABLE vehicles ADD UNIQUE (license_plate);
+ALTER TABLE users ADD UNIQUE (vat);
+ALTER TABLE roles ADD UNIQUE (company_id, function);
+ALTER TABLE fleets ADD UNIQUE (company_id, name);
+ALTER TABLE permissions ADD UNIQUE (name);
