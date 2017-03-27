2516852f3fae45fe0a90dccd66d7a63cc28c5833
nnmak <nils.mak@gmail.com>
2017-03-27 18:02:54 +0200

    converted FormChoice, InfoTable, DetailTable to stateless

--- frontend/src/javascripts/components/tables/InfoTable.tsx
+++ frontend/src/javascripts/components/tables/InfoTable.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import T     from 'i18n-react';
 
-class InfoTable extends React.Component<Table.Info.Props, {}> {
+interface Props {
+      head    : Table.Head.Data[];
+      data    : any;
+      onClick : (e : any) => void;
+}
 
-  public getRows() : any {
-    var { head, data } = this.props;
+const InfoTable : React.StatelessComponent<Props> = props => {
 
-    return data.map((item : any, i : number) => {
+    var { head, data, onClick } = props;
+ 
+    const tableHead = head.map((headData : Table.Head.Data) =>
+    (
+      <th key={ headData.key } scope='row' className='table-row col-sm-3' >{ T.translate(headData.label) }</th>
+    ));
+
+    const tableRows = data.map((item : any, i : number) => {
       const cells = head.map((headData : Table.Head.Data, j : number) => {
         return (
           <td key={ j }>{ item[headData.key] }</td>
         );
       });
       return (
-        <tr key={ i } onClick={ () => this.props.onClick(item.id) } className='table-row'>{cells}</tr>
+        <tr key={ i } onClick={ () => onClick(item.id) } className='table-row'>{cells}</tr>
       );
     });
-  }
-
-  render() {
-    const tableHead = this.props.head.map((headData : Table.Head.Data) =>
-    (
-      <th key={ headData.key } scope='row' className='table-row col-sm-3' >{ T.translate(headData.label) }</th>
-    ));
-
-    const tableRows = this.getRows();
 
     return (
       <div className='table-wrap'>
@@ -38,7 +39,6 @@ class InfoTable extends React.Component<Table.Info.Props, {}> {
       </table>
       </div>
     );
-  }
 }
 
 export default InfoTable;
--- frontend/src/javascripts/types/form.d.ts
+++ frontend/src/javascripts/types/form.d.ts
@@ -9,15 +9,6 @@ namespace Form {
     }
   }
 
-  namespace Choice {
-    export interface Props {
-      placeholder : string;
-      choices : Table.Head.Data[];
-      callback    : (e : any) => void;
-      value : string;
-    }
-  }
-
   export interface Error {
     field : string;
     error : string;
