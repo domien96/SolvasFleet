8b6523f060d25a22e91f3e249b5d6c59f97736b5
sjabasti <Steven.Bastiaens@UGent.be>
2017-03-27 12:05:20 +0200

    add archive to other filters

--- backend/src/main/java/solvas/rest/query/RoleFilter.java
+++ backend/src/main/java/solvas/rest/query/RoleFilter.java
@@ -13,14 +13,14 @@ import java.util.List;
 /**
  * @author Niko Strijbol
  */
-public class RoleFilter implements Filter<Role> {
+public class RoleFilter extends ArchiveFilter<Role> {
 
     private int company = -1;
     private int user = -1;
 
     @Override
     public Collection<Predicate> asPredicates(CriteriaBuilder builder, Root<Role> root) {
-        List<Predicate> predicates = new ArrayList<>();
+        Collection<Predicate> predicates = super.asPredicates(builder,root);
 
         if (company >= 0) {
             predicates.add(builder.equal(root.get("company"), company));
