a10434cd3e23c017bcdb78447f39bb1dc3a1220e
benji <benj_cous@hotmail.com>
2017-03-27 15:14:37 +0200

    Refactor Clients

--- frontend/src/javascripts/components/clients/Clients.tsx
+++ frontend/src/javascripts/components/clients/Clients.tsx
@@ -1,43 +1,9 @@
 import React from 'react';
-import { browserHistory, Link } from'react-router';
+import { browserHistory } from'react-router';
 
-import Card       from '../app/Card.tsx';
-import Header     from '../app/Header.tsx';
-import InfoTable  from '../tables/InfoTable.tsx';
+import Layout from './Layout.tsx';
 
 import { fetchClients } from '../../actions/client_actions.ts';
-import { th } from '../../utils/utils.ts';
-
-
-interface OverviewProps {
-  clients: Company[];
-}
-
-class Overview extends React.Component<OverviewProps, {}> {
-
-  constructor() {
-    super();
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  public handleClick(id : number) {
-    browserHistory.push('/clients/' + id);
-  }
-
-  render() {
-    const tableHead = [
-      th('id', 'company.id'),
-      th('name', 'company.name'),
-      th('vatNumber', 'company.vatNumber'),
-      th('phoneNumber', 'company.phoneNumber'),
-    ];
-
-    return (
-      <InfoTable head={ tableHead } data={ this.props.clients } onClick={ this.handleClick } />
-    );
-  }
-}
-
 
 class Clients extends React.Component<{}, Companies.State> {
 
@@ -52,28 +18,13 @@ class Clients extends React.Component<{}, Companies.State> {
     });
   }
 
+  handleClick(id : number) {
+    browserHistory.push('/clients/' + id);
+  }
+
   render() {
     return (
-      <div>
-        <Header>
-          <h2>Clients</h2>
-        </Header>
-        <div className='wrapper'>
-          <div className='row'>
-            <div className='col-xs-12 col-md-12'>
-              <Card>
-                <div className='card-content'>
-                  <Link to='/clients/new' className='btn btn-default pull-right'>
-                    <span className='glyphicon glyphicon-plus' aria-hidden='true'></span>
-                    Add new client
-                  </Link>
-                  <Overview clients={ this.state.clients } />
-                </div>
-              </Card>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Layout clients={ this.state.clients } onClientSelect={ this.handleClick } />
     );
   }
 }
