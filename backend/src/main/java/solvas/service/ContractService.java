3e2e342dc44104489658817996cf06474a7cbc94
sjabasti <Steven.Bastiaens@UGent.be>
2017-03-31 01:24:05 +0200

    fixed compilation error

--- backend/src/main/java/solvas/service/ContractService.java
+++ backend/src/main/java/solvas/service/ContractService.java
@@ -6,20 +6,20 @@ import solvas.persistence.api.Dao;
 import solvas.persistence.api.Filter;
 import solvas.rest.api.models.ApiContract;
 import solvas.service.mappers.AbstractMapper;
-import solvas.service.models.Contract;
+import solvas.service.models.Insurance;
 
 /**
  * Service class of Contracts and contracttypes.
  * Created by domien on 30/03/2017.
  */
-public class ContractService extends AbstractService<Contract,ApiContract> {
+public class ContractService extends AbstractService<Insurance,ApiContract> {
     /**
      * Contruct an abstractservice
      *
      * @param modelDao the DAO of the model
      * @param mapper   the mapper between the apimodel and the model
      */
-    public ContractService(Dao<Contract> modelDao, AbstractMapper<Contract, ApiContract> mapper) {
+    public ContractService(Dao<Insurance> modelDao, AbstractMapper<Insurance, ApiContract> mapper) {
         super(modelDao, mapper);
     }
 
@@ -29,7 +29,7 @@ public class ContractService extends AbstractService<Contract,ApiContract> {
      * @param filters The filters.
      * @return the paged representation of the contracttype objects
      */
-    public Page<ApiContract> findAllContractTypes(Pageable pagination, Filter<Contract> filters) {
+    public Page<ApiContract> findAllContractTypes(Pageable pagination, Filter<Insurance> filters) {
         return null; // TODO dao call
     }
 }
