b58c75529c1bde5d0e8da21e9578c27ba1c4e9a3
Domien Van Steendam <domien.vansteendam@ugent.be>
2017-03-31 00:26:02 +0200

    Added contractcontroller

---
+++ backend/src/main/java/solvas/rest/query/ContractFilter.java
@@ -0,0 +1,10 @@
+package solvas.rest.query;
+
+import solvas.service.models.Contract;
+
+/**
+ * Created by domien on 30/03/2017.
+ */
+public class ContractFilter extends ArchiveFilter<Contract> {
+    //TODO
+}
