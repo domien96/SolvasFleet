cf8840991da6cc13112cbc8971f3cd5bc7e1d2f7
Niko Strijbol <strijbol.niko@gmail.com>
2017-04-01 19:48:48 +0200

    The contract/types don't need to support pagination

--- backend/src/main/java/solvas/service/InsuranceService.java
+++ backend/src/main/java/solvas/service/InsuranceService.java
@@ -1,23 +1,24 @@
 package solvas.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import solvas.persistence.api.Dao;
 import solvas.persistence.api.DaoContext;
-import solvas.persistence.api.Filter;
 import solvas.persistence.api.dao.InsuranceTypeDao;
 import solvas.rest.api.models.ApiInsurance;
-import solvas.service.mappers.AbstractMapper;
 import solvas.service.models.Insurance;
 import solvas.service.models.InsuranceType;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 /**
- * Created by domien on 1/04/2017.
+ * Service for insurances.
+ *
+ * @author Domien
  */
 @Service
-public class InsuranceService extends AbstractService<Insurance,ApiInsurance> {
+public class InsuranceService extends AbstractService<Insurance, ApiInsurance> {
+
     private final InsuranceTypeDao insuranceTypeDao;
 
     /**
@@ -32,12 +33,14 @@ public class InsuranceService extends AbstractService<Insurance,ApiInsurance> {
     }
 
     /**
-     * Find all different possible contract types.
-     * @param pagination The pagination information.
-     * @param filters The filters.
-     * @return the paged representation of the contracttypes as strings
+     * Find all contract types.
+     *
+     * @return The contract types.
      */
-    public Page<String> findAllInsuranceTypes(Pageable pagination, Filter<InsuranceType> filters) {
-        return insuranceTypeDao.findAll(filters,pagination).map(s -> s.getName());
+    public Collection<String> findAllInsuranceTypes() {
+        return insuranceTypeDao.findAll()
+                .stream()
+                .map(InsuranceType::getName)
+                .collect(Collectors.toList());
     }
 }
