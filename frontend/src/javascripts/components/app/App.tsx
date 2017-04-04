bb9b962bb863bbffad7a1111fa93d3237fb12338
nnmak <nils.mak@gmail.com>
2017-04-04 09:36:38 +0200

    mozilla height fix

--- frontend/src/javascripts/components/app/App.tsx
+++ frontend/src/javascripts/components/app/App.tsx
@@ -58,7 +58,7 @@ class App extends React.Component<Props, {}> {
 
   render() {
     return (
-      <div id='wrapper'>
+      <div id='wrapper' className='wrapper'>
         <Sidebar />
         <div className='page-wrapper'>
           { this.props.children }
