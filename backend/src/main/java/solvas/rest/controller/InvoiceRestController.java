620864689344a4ed509b7892f07b0eb015438c07
sjabasti <Steven.Bastiaens@UGent.be>
2017-04-15 15:39:36 +0200

    added controller endpoints for invoice

---
+++ backend/src/main/java/solvas/rest/controller/InvoiceRestController.java
@@ -0,0 +1,120 @@
+package solvas.rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import solvas.rest.api.models.ApiModel;
+import solvas.rest.query.InvoiceFilter;
+import solvas.service.InvoiceService;
+import solvas.service.models.Model;
+
+import javax.validation.Valid;
+
+
+/**
+ * Rest controller for Invoice
+ *
+ */                                                                //TODO REPLACE
+public class InvoiceRestController  extends AbstractRestController<Model,ApiModel> {
+
+    /**
+     * Default constructor.
+     *
+     * @param service service class for entities
+     */
+    @Autowired
+    public InvoiceRestController(InvoiceService service) {
+        super(service);
+    }
+
+    /**
+     * Query all models, accounting for pagination settings and respect the filters. The return value of this
+     * method will contain an object, according to the API spec.
+     *
+     * @param pagination The pagination information.
+     * @param filter The filters.
+     * @param result The validation results of the filterResult
+     *
+     * @return ResponseEntity
+     */
+    @RequestMapping(value = "/invoices", method = RequestMethod.GET)
+    public ResponseEntity<?> listAll(Pageable pagination, InvoiceFilter filter, BindingResult result) {
+        return super.listAll(pagination, filter, result);
+    }
+
+
+    @Override
+    @RequestMapping(value = "/invoices/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getById(@PathVariable int id) {
+        return super.getById(id);
+    }
+
+
+
+    /**
+     * Get the active invoice for a company.
+     *
+     * @param id The ID of the company.
+     * @param pagination The pagination.
+     * @param filter The filters.
+     * @param result The validation results.
+     *
+     * @return The response.
+     */
+    @RequestMapping(value = "/companies/{id}/invoices/current", method = RequestMethod.GET)
+    public ResponseEntity<?> getActiveByCompanyId(@PathVariable int id,Pageable pagination, InvoiceFilter filter, BindingResult result) {
+        //Todo filter set active
+        //Todo filter set company/fleet
+        //filter.setFleet(id);
+        return super.listAll(pagination,filter,result);
+    }
+
+
+    /**
+     * Get invoice with id
+     *
+     * @param id The ID of the invoice.
+     *
+     * @return The response.
+     */
+    @RequestMapping(value = "/companies/{company_id}/invoices/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getByCompanyAndInvoiceId(@PathVariable int company_id, @PathVariable int id) {
+        return super.getById(id);
+    }
+
+    /**
+     * Get invoice with id
+     *
+     * @param id The ID of the invoice.
+     *
+     * @return The response.
+     */
+    @RequestMapping(value = "/companies/{company_id}/invoices/{id}{extension}", method = RequestMethod.GET)
+    public ResponseEntity<?> getByCompanyAndInvoiceIdWithExtension(@PathVariable int company_id, @PathVariable int id,@PathVariable String extension) {
+        //Todo process extension
+        return super.getById(id);
+    }
+
+
+    /**
+     * Get all invoices for a company.
+     *
+     * @param id The ID of the company.
+     * @param pagination The pagination.
+     * @param filter The filters.
+     * @param result The validation results.
+     *
+     * @return The response.
+     */
+    @RequestMapping(value = "/companies/{id}/invoices", method = RequestMethod.GET)
+    public ResponseEntity<?> getByCompanyId(@PathVariable int id,Pageable pagination, InvoiceFilter filter, BindingResult result) {
+        //Todo filter set active
+        //Todo filter set company/fleet
+        //filter.setFleet(id);
+        return super.listAll(pagination,filter,result);
+    }
+
+
+}
---
+++ backend/src/main/java/solvas/rest/query/InvoiceFilter.java
@@ -0,0 +1,31 @@
+package solvas.rest.query;
+
+import solvas.service.models.Model;
+
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.Collection;
+
+/**
+ * Created by steve on 15/04/2017.
+ */
+public class InvoiceFilter extends ArchiveFilter<Model> {
+
+    private int fleet = -1;
+
+    @Override
+    public Collection<Predicate> asPredicates(CriteriaBuilder builder, Root<Model> root) {
+        Collection<Predicate> predicates = super.asPredicates(builder,root);
+        if (fleet >= 0) {
+            predicates.add(builder.equal(root.get("fleet"), fleet));
+        }
+        return predicates;
+    }
+
+    public void setFleet(int fleet) {
+        this.fleet = fleet;
+    }
+}
+
