6f164c259c93476f83e739035c6d9d70935c47f5
Domien Van Steendam <domien.vansteendam@ugent.be>
2017-04-16 21:26:12 +0200

    belongs to previous commit

--- backend/src/main/java/solvas/persistence/api/EntityNotFoundException.java
+++ backend/src/main/java/solvas/persistence/api/EntityNotFoundException.java
@@ -4,4 +4,10 @@ package solvas.persistence.api;
  * Exception is thrown when user searches for an entity which does not exist (example: non-existent id)
  */
 public class EntityNotFoundException extends Exception {
+
+    public EntityNotFoundException() { super();}
+
+    public EntityNotFoundException(String s) {
+        super(s);
+    }
 }
