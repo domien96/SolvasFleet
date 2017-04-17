ebfae0743d2aed48434fa70468886eee6eead532
Domien Van Steendam <domien.vansteendam@ugent.be>
2017-04-17 23:51:46 +0200

    Pdf build code for Invoices.

--- backend/src/main/java/solvas/rest/invoices/pdf/InvoicePdfView.java
+++ backend/src/main/java/solvas/rest/invoices/pdf/InvoicePdfView.java
@@ -2,7 +2,11 @@ package solvas.rest.invoices.pdf;
 
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
@@ -11,17 +15,26 @@ import solvas.service.models.Invoice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Field;
 import java.util.Map;
 
 @Component
 public class InvoicePdfView extends AbstractITextPdfView {
 
+    private Font dataFont = new Font(Font.FontFamily.HELVETICA,12,Font.NORMAL);
+    private Font dataFontBold = new Font(Font.FontFamily.HELVETICA,12,Font.BOLD);
+
     @Override
     protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("sdqjg");
         ApiInvoice invoice = (ApiInvoice) model.get("invoice");
         // get data model which is passed by the Spring container
 
-        document.add(new Paragraph("Recommended books for Spring framework"));
+        document.add(new Paragraph(String.format("Invoice overview (id : %s", invoice.getId())));
+        PdfPTable table = new PdfPTable(2);
+        for (Field f : invoice.getClass().getDeclaredFields()) {
+            table.addCell(new PdfPCell(new Phrase(f.getName(), dataFontBold)));
+            table.addCell(new PdfPCell(new Phrase(f.get(invoice).toString(), dataFont)));
+        }
+        document.add(table);
     }
 }
