6cae1880b7a4364f0ad88b5c761a9e9a504abe76
Domien Van Steendam <domien.vansteendam@ugent.be>
2017-04-17 16:42:36 +0200

    Unnecessary where clause

--- backend/src/main/resources/db/migration/V2_12__remove_subfleets.sql
+++ backend/src/main/resources/db/migration/V2_12__remove_subfleets.sql
@@ -1,6 +1,6 @@
 ALTER TABLE  fleet_subscriptions ADD COLUMN fleet_id INT REFERENCES fleets(fleet_id);
 -- now fill in the fleet id using the subfleet id
-UPDATE fleet_subscriptions f SET fleet_id = (SELECT sf.fleet_id FROM subfleets sf JOIN fleets USING (fleet_id) WHERE sf.subfleet_id = f.subfleet_id ) WHERE subfleet_id >= 1;
+UPDATE fleet_subscriptions f SET fleet_id = (SELECT sf.fleet_id FROM subfleets sf JOIN fleets USING (fleet_id) WHERE sf.subfleet_id = f.subfleet_id );
 
 ALTER TABLE fleet_subscriptions ALTER COLUMN fleet_id SET NOT NULL;
 
