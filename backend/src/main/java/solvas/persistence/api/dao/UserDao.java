2f2c6343592b2b676d4b077f80479fd45f40ea79
David Vandorpe <david.vandorpe@ugent.be>
2017-04-17 13:11:22 +0200

    Fixed user not found

--- backend/src/main/java/solvas/persistence/api/dao/UserDao.java
+++ backend/src/main/java/solvas/persistence/api/dao/UserDao.java
@@ -1,6 +1,7 @@
 package solvas.persistence.api.dao;
 
 import org.springframework.stereotype.Repository;
+import solvas.persistence.api.EntityNotFoundException;
 import solvas.service.models.User;
 import solvas.persistence.api.Dao;
 
@@ -19,4 +20,19 @@ public interface UserDao extends Dao<User> {
      * @return The associated user entity
      */
     User getByEmail(String email);
+
+    /**
+     * Find a single user by his/her email
+     * @param email The email to search for
+     * @return The associated user entity
+     * @throws EntityNotFoundException User not found
+     */
+    default User findByEmail(String email) throws EntityNotFoundException {
+        User u = getByEmail(email);
+        if(u == null) {
+            throw new EntityNotFoundException(String.format("Could not find user with email %s.", email));
+        }
+
+        return u;
+    }
 }
