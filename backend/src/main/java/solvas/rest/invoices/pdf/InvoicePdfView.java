b221c599dc86c960962cbdb47873203c1178177d
Domien Van Steendam <domien.vansteendam@ugent.be>
2017-04-18 00:13:25 +0200

    typo & layout

--- backend/src/main/java/solvas/rest/invoices/pdf/InvoicePdfView.java
+++ backend/src/main/java/solvas/rest/invoices/pdf/InvoicePdfView.java
@@ -1,10 +1,7 @@
 package solvas.rest.invoices.pdf;
 
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Font;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.Phrase;
+import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
@@ -32,18 +29,20 @@ public class InvoicePdfView extends AbstractITextPdfView {
     protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer, HttpServletRequest request, HttpServletResponse response) throws Exception {
         ApiInvoice invoice = (ApiInvoice) model.get("invoice");
         // get data model which is passed by the Spring container
-
-        document.add(new Paragraph(String.format("Invoice overview (id : %s", invoice.getId())));
+        Paragraph p = new Paragraph(String.format("Invoice overview (id : %s)\n", invoice.getId()));
+        p.setAlignment(Element.ALIGN_CENTER);
+        document.add(p);
         PdfPTable table = new PdfPTable(2);
         for (Field f : invoice.getClass().getDeclaredFields()) {
             table.addCell(new PdfPCell(new Phrase(f.getName(), dataFontBold)));
             table.addCell(new PdfPCell(new Phrase(getGetterMethodByField(f).invoke(invoice).toString(), dataFont)));
         }
+        table.setHorizontalAlignment(Element.ALIGN_CENTER);
         document.add(table);
     }
 
     private Method getGetterMethodByField(Field f) throws IntrospectionException {
-        PropertyDescriptor pd = new PropertyDescriptor(f.getName(), Invoice.class);
+        PropertyDescriptor pd = new PropertyDescriptor(f.getName(), ApiInvoice.class);
         return pd.getReadMethod();
     }
 }
