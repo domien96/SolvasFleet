c52949df2e3a14dccaa161cea057a4ded9856b29
Domien Van Steendam <domien.vansteendam@ugent.be>
2017-04-17 17:46:42 +0200

    Bugfix migration

--- backend/src/main/resources/db/migration/V2_11__invoice_fix.sql
+++ backend/src/main/resources/db/migration/V2_11__invoice_fix.sql
@@ -1,9 +1,14 @@
-ALTER TABLE invoices DROP COLUMN type;
-
+-- enum values in caps
+CREATE TYPE invoice_type_caps AS ENUM ('BILLING','PAYMENT');
+ALTER TABLE invoices RENAME COLUMN type TO old_type;
+ALTER TABLE invoices ADD COLUMN type invoice_type_caps;
+UPDATE invoices SET type = 'BILLING' WHERE old_type = 'billing';
+UPDATE invoices SET type = 'PAYMENT' WHERE old_type = 'payment';
+ALTER TABLE invoices ALTER COLUMN type SET NOT NULL;
+ALTER TABLE invoices DROP COLUMN old_type;
 DROP TYPE invoice_type;
-CREATE TYPE invoice_type AS ENUM ('BILLING','PAYMENT');
 
-ALTER TABLE invoices ADD COLUMN type invoice_type NOT NULL;
 
+-- renaming
 ALTER TABLE invoices RENAME COLUMN startDate TO start_date;
 ALTER TABLE invoices RENAME COLUMN endDate TO end_date;
