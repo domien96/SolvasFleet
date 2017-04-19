bb82c82132b805146cabd6f2a39893c8a63d1d7e
nnmak <nils.mak@gmail.com>
2017-04-19 19:23:27 +0200

    changed password placeholder to ****** in EditUserForm

--- frontend/src/javascripts/components/user_form/form/Info.tsx
+++ frontend/src/javascripts/components/user_form/form/Info.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-import FormField from './UFormField.tsx';
+import FormField from '../../forms/FormField.tsx';
 import Card from '../../app/Card.tsx';
 
 export interface Props {
   handleChange : (field : string, e : any) => void;
-  hasError : (s : string) => boolean;
+  hasError : (e : any) => boolean;
   user : UserData;
 }
 
 const Info : React.StatelessComponent<Props> = (props) => {
+  let handleChange = (field : User.Field) => {
+    return (e : any) => {
+      props.handleChange(field, e);
+    }
+  }
+
+  var { firstName, lastName, email, password } = props.user;
+  const pwLabel = props.user.password==undefined ? '******' : password;
+
   return (
     <div className='col-xs-12 col-md-7'>
       <Card>
@@ -17,10 +26,10 @@ const Info : React.StatelessComponent<Props> = (props) => {
           <h5>General info</h5>
         </div>
         <div className='card-content'>
-          <FormField field='firstName' type='text' { ...props } />
-          <FormField field='lastName' type='text' { ...props } />
-          <FormField field='email' type='email' { ...props } />
-          <FormField field='password' type='password' { ...props } />
+          <FormField value={ firstName } placeholder='user.firstName' type='text'     callback={ handleChange('firstName') } hasError={ props.hasError('firstName')} />
+          <FormField value={ lastName } placeholder='user.lastName' type='text'     callback={ handleChange('lastName') } hasError={ props.hasError('lastName')}  />
+          <FormField value={ email } placeholder='user.email' type='email'    callback={ handleChange('email') } hasError={ props.hasError('email')}  />
+          <FormField value={ pwLabel } placeholder='user.password' type='password' callback={ handleChange('password') } hasError={ props.hasError('password')}  />
         </div>
       </Card>
     </div>
