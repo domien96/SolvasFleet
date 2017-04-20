93e5498f5e8af225ff148383b4f6b1390017be5b
David Vandorpe <david.vandorpe@ugent.be>
2017-04-20 16:06:32 +0200

    Use import

--- backend/src/main/java/solvas/persistence/hibernate/dao/FleetSubscriptionDaoImpl.java
+++ backend/src/main/java/solvas/persistence/hibernate/dao/FleetSubscriptionDaoImpl.java
@@ -1,6 +1,7 @@
 package solvas.persistence.hibernate.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
 import solvas.persistence.api.dao.FleetSubscriptionDao;
@@ -35,7 +36,7 @@ public class FleetSubscriptionDaoImpl implements FleetSubscriptionDaoCustom {
     public Optional<FleetSubscription> activeForVehicle(Vehicle vehicle) {
         TypedQuery<FleetSubscription> q = entityManager.createQuery("select s from FleetSubscription s where s.vehicle = ?1 and s.startDate <= current_date and (s.endDate is null or s.endDate >= current_date) and s.archived = false", FleetSubscription.class)
                 .setParameter(1, vehicle);
-        return Optional.ofNullable(org.springframework.dao.support.DataAccessUtils.singleResult(q.getResultList()));
+        return Optional.ofNullable(DataAccessUtils.singleResult(q.getResultList()));
     }
     @Override
     public Collection<FleetSubscription> findByFleetAndStartDateAndEndDate(Fleet fleet, LocalDate start, LocalDate end) {
