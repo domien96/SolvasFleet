3925acf158479d9074f155b44aa22f2a473f9f0f
nnmak <nils.mak@gmail.com>
2017-04-20 11:49:02 +0200

    forgot to add invoices

---
+++ frontend/src/javascripts/components/invoices/Invoices.tsx
@@ -0,0 +1,59 @@
+import React              from 'react';
+
+import { fetchInvoices } from '../../actions/fleet_actions.ts';
+import { redirect_to } from'../../router.tsx';
+import InvoicesView from './InvoicesView.tsx'
+
+interface Props {
+  [ params : string ] : { [ fleetId : string ] : number };
+}
+
+interface State {
+  invoices : InvoiceData[];
+}
+
+class Invoices extends React.Component<Props, State> {
+
+  constructor(props : {}) {
+    super(props);
+    this.state = { invoices: [] };
+  }
+
+  componentDidMount() {
+    if(this.props.params.id){
+      this.fetchInvoices(this.props.params.id);
+    }
+  }
+
+  componentWillReceiveProps(nextProps : any){
+    if (nextProps.params.id != this.props.params.id){
+      this.fetchInvoices(nextProps.params.id);
+    }
+  }
+
+  fetchInvoices(fleetId : number) {
+    fetchInvoices(fleetId, ((data : Invoices.Data) => {
+      this.setState({ invoices: data.data })
+    }));
+  }
+
+  handleClick(id : number) {
+    redirect_to(`fleets/${this.props.params.fleetId}/invoices/${id}`);
+  }
+
+  render() {
+    const children = React.Children.map(this.props.children,
+      (child : any) => React.cloneElement(child, {
+        fetchInvoices: this.fetchInvoices.bind(this)
+      })
+    );
+
+    return (
+      <InvoicesView invoices={ this.state.invoices } onInvoiceSelect={ this.handleClick } >
+        { children }
+      </InvoicesView>
+    );
+  }
+}
+
+export default Invoices;
