c902ddb999024c55e45540d91c9e5e5b290ade08
Domien Van Steendam <domien.vansteendam@ugent.be>
2017-04-21 18:47:21 +0200

    - Current invoice pdf - Fixed invoice url

--- backend/src/main/java/solvas/service/mappers/InvoiceMapper.java
+++ backend/src/main/java/solvas/service/mappers/InvoiceMapper.java
@@ -3,6 +3,7 @@ package solvas.service.mappers;
 import org.springframework.stereotype.Component;
 import solvas.persistence.api.DaoContext;
 import solvas.persistence.api.EntityNotFoundException;
+import solvas.rest.SimpleUrlBuilder;
 import solvas.rest.api.models.ApiInvoice;
 import solvas.service.mappers.exceptions.DependantEntityNotFound;
 import solvas.service.models.Invoice;
@@ -17,7 +18,7 @@ import java.math.BigDecimal;
 @Component
 public class InvoiceMapper extends AbstractMapper<Invoice,ApiInvoice> {
 
-    private static final String ROOTPATH="/invoices/";
+    private static final String ROOTPATH="fleets/{id}/invoices/";
 
     /**
      * Create a mapper between invoice and Apiinvoice
@@ -47,7 +48,7 @@ public class InvoiceMapper extends AbstractMapper<Invoice,ApiInvoice> {
         api.setTotalAmount(model.getAmount().longValue());
         api.setType(model.getType().getText());
         api.setFleet(model.getFleet().getId());
-        api.setUrl(ROOTPATH);
+        api.setUrl(SimpleUrlBuilder.buildUrlFromBase(ROOTPATH+"{invoice_id}",model.getFleet().getId(),model.getId()));
         return api;
     }
 
