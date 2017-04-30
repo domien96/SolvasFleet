320f94ac55d8eee8c854d0eb4e00687510dbf262
sjabasti <Steven.Bastiaens@UGent.be>
2017-04-30 17:43:18 +0200

    some tests

--- backend/src/itest/java/solvas/persistence/api/dao/CompanyDaoTest.java
+++ backend/src/itest/java/solvas/persistence/api/dao/CompanyDaoTest.java
@@ -2,13 +2,15 @@ package solvas.persistence.api.dao;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import solvas.service.mappers.exceptions.FieldNotFoundException;
 import solvas.service.models.Company;
 
-import java.lang.reflect.Field;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Integration tests for custom behaviour on the company DAO.
@@ -34,72 +36,29 @@ public class CompanyDaoTest extends DaoTest {
         // Test existing
         assertThat(companyDao.findByName(company.getName()), hasSize(1));
         // Save and test again.
-        Company newCompany = new Company();
-        newCompany.setVatNumber("TestVAT");
-        copyAttributes(newCompany,company,"name", "phoneNumber","addressCity","addressCountry","addressHouseNumber",
-                "addressPostalCode","addressStreet","type");
-
-        companyDao.save(newCompany);
+        company.setVatNumber("TestVAT");
+        company.setId(0);
+        companyDao.save(company);
         assertThat(companyDao.findByName(company.getName()), hasSize(2));
     }
 
     /**
-     * Copy attribute from source (if set), to target
-     *
-     * @param target     Entity to copy to
-     * @param src        Entity to copy from
-     * @param attributes attributes to copy
-     * @throws FieldNotFoundException If a field wasn't found or was inaccessible
+     * Test finding companies with a certain vat.
      */
+    @Test
+    public void withVat() {
+        List<Company> companies = companyDao.findAll();
+        companies.forEach(company -> assertThat(companyDao.findByVatNumber(company.getVatNumber()).get().getId(),is(company.getId())));
 
-    private void copyAttributes(Object target, Object src, String... attributes) {
-        for (String attribute : attributes) {
-            copyNotNull(target, src, attribute);
-        }
     }
 
 
     /**
-     * Searches a field of a class, if it is not found, it will search in its superclasses
-     * @param clazz base class of which the field must be searched
-     * @param name attribute to copy
-     * @return Field with name
-     * @throws FieldNotFoundException If field wasn't found
+     * Test finding companies with a certain vat, which will throw an error
      */
-    private Field findFieldInSuper(Class<?> clazz, String name) {
-        Class<?> current = clazz;
-        do {
-            try {
-                return current.getDeclaredField(name);
-            } catch (NoSuchFieldException ignored) {}
-        } while((current = current.getSuperclass()) != null);
-        throw new FieldNotFoundException("Field "+name+"not found");
-    }
-
-
-    /**
-     * Copy attribute from source (if set), to target
-     *
-     * @param target Entity to copy to
-     * @param source Entity to copy from
-     * @param name   attribute to copy
-     * @throws FieldNotFoundException If field wasn't found or was inaccessible
-     */
-    private void copyNotNull(Object target, Object source, String name) {
-        Field targetField = findFieldInSuper(target.getClass(),name);
-        Field sourceField = findFieldInSuper(source.getClass(),name);
-        try {
-            targetField.setAccessible(true);
-            sourceField.setAccessible(true);
-            if (sourceField.get(source) != null) {
-                targetField.set(target, sourceField.get(source));
-            }
-        } catch (IllegalAccessException e) {
-            throw new FieldNotFoundException(e);
-        } finally {
-            targetField.setAccessible(false);
-            sourceField.setAccessible(false);
-        }
+    @Test(expected=NoSuchElementException.class)
+    public void withVatError() {
+        companyDao.findByVatNumber("").get();
     }
 
 }
---
+++ backend/src/itest/java/solvas/persistence/api/dao/ContractDaoTest.java
@@ -0,0 +1,18 @@
+package solvas.persistence.api.dao;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Integration tests for custom behaviour on the contract DAO.
+ *
+ * @author Sjabasti
+ */
+public class ContractDaoTest extends DaoTest {
+
+    @Autowired
+    private ContractDao contractDao;
+
+
+
+}
