1715afd0e67e2ff3b9816719626417d7439d9d4d
klivdnbu <karel.vandenbussche@ugent.be>
2017-05-08 19:50:53 +0200

    add tests for user, users and vehicle

---
+++ frontend/src/__tests__/components/users.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {mount,shallow} from 'enzyme';
+
+import Layout from '../../javascripts/components/users/Layout.tsx';
+
+test('Render users layout correctly',()=>{
+  var layout = shallow(<Layout response="aa"><h1>TEST</h1></Layout>);
+  expect(layout.find('h2').text()).toEqual('user.users');
+  expect(layout.find('Listing').prop('addNewRoute')).toEqual('/users/new');
+  expect(layout.find('Listing').prop('modelName')).toEqual('user');
+  expect(layout.find('Listing').prop('response')).toEqual("aa");
+  //Check if child components reside in the layout
+  expect(layout.containsMatchingElement(<h1>TEST</h1>)).toBeTruthy();
+})
+
+import Users from '../../javascripts/components/users/Users.tsx';
+
+jest.mock('../../javascripts/actions/user_actions.ts', () => ({
+  fetchUsers: jest.fn().mockImplementation((b)=>b({"data":[{"id":"1","firstName":"bob","lastName":"b","email":"a","password":"bb"}]}))
+}));
+test('Users renders correctly',()=>{
+  var users = mount(<Users/>);
+  expect(users.find('Layout').prop('response')).toEqual({"data":[{"id":"1","firstName":"bob","lastName":"b","email":"a","password":"bb"}]});
+})
