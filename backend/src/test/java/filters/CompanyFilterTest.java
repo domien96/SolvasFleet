533e501732a90a97c256083e9981b474d85cce5a
Niko Strijbol <strijbol.niko@gmail.com>
2017-05-11 14:51:26 +0200

    Document test class (SonarQube I)

--- backend/src/test/java/filters/CompanyFilterTest.java
+++ backend/src/test/java/filters/CompanyFilterTest.java
@@ -4,6 +4,10 @@ import solvas.rest.query.ArchiveFilter;
 import solvas.rest.query.CompanyFilter;
 import solvas.service.models.Company;
 
+/**
+ * Test the {@link CompanyFilter}. This will test that every constraint results in a constraint. This does not
+ * actually test if the constraints are correct, that is something for an integration test.
+ */
 public class CompanyFilterTest extends AbstractFilterTest<Company> {
     private CompanyFilter filter = new CompanyFilter();
 
