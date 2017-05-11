28911cf58be5c418757a4f909287ec0efce8a74a
Niko Strijbol <strijbol.niko@gmail.com>
2017-05-11 21:26:14 +0200

    Better init with EventListener

--- backend/src/main/java/solvas/service/audit/AuditInterceptor.java
+++ backend/src/main/java/solvas/service/audit/AuditInterceptor.java
@@ -2,47 +2,42 @@ package solvas.service.audit;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.BeansException;
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.type.Type;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import solvas.persistence.api.DaoContext;
+import solvas.persistence.api.EntityNotFoundException;
+import solvas.service.models.*;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Iterator;
 
-import org.hibernate.EmptyInterceptor;
-import org.hibernate.type.Type;
-import solvas.persistence.api.DaoContext;
-import solvas.persistence.api.EntityNotFoundException;
-import solvas.service.models.*;
-
+/**
+ * Intercepts changes made to the database, and logs them with additional metadata, such as the current user.
+ *
+ * @author Steven
+ */
 @Component
-public class AuditInterceptor extends EmptyInterceptor  implements BeanFactoryAware{
-
+public class AuditInterceptor extends EmptyInterceptor {
 
     private DaoContext daoContext;
-
-
-    private BeanFactory beanFactory;
-
     private MapperContext mapperContext;
-
-    private HashMap<Object,Revision> transactionRevisions = new HashMap<>();
-
+    private HashMap<Object, Revision> transactionRevisions = new HashMap<>();
     private ObjectMapper objectMapper;
 
-    private User getAuthenticatedUser(){
+    private User getAuthenticatedUser() {
         try {
             return daoContext.getUserDao().findByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
         } catch (EntityNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
-
-
 
     @Override
     public boolean onFlushDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
@@ -58,7 +53,7 @@ public class AuditInterceptor extends EmptyInterceptor  implements BeanFactoryAw
 
 
         // Connect revision with a entity
-        transactionRevisions.put(entity,revision);
+        transactionRevisions.put(entity, revision);
         return false; // We do not make changes
     }
 
@@ -75,7 +70,7 @@ public class AuditInterceptor extends EmptyInterceptor  implements BeanFactoryAw
 
 
         // Connect revision with a entity
-        transactionRevisions.put(entity,revision);
+        transactionRevisions.put(entity, revision);
         return false; // We do not make changes
     }
 
@@ -92,13 +87,13 @@ public class AuditInterceptor extends EmptyInterceptor  implements BeanFactoryAw
         revision.setMethod(MethodType.DELETE);
 
         // Connect revision with a entity
-        transactionRevisions.put(entity,revision);
+        transactionRevisions.put(entity, revision);
     }
 
-    private Revision getPayloadAndId(Object entity, Revision revision){
+    private Revision getPayloadAndId(Object entity, Revision revision) {
         Object about = entity;
         if (entity instanceof FleetSubscription) {
-            about =  ((FleetSubscription) entity).getVehicle();
+            about = ((FleetSubscription) entity).getVehicle();
             revision.setMethod(MethodType.UPDATE); // Fleet subscriptions are updates to the vehicle
         }
         try {
@@ -106,48 +101,45 @@ public class AuditInterceptor extends EmptyInterceptor  implements BeanFactoryAw
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e); // Can we even catch an exception at this point
         }
-        revision.setEntity(((Model)entity).getId());
+        revision.setEntity(((Model) entity).getId());
         return revision;
 
     }
 
-
     @Override
     public void postFlush(Iterator entities) {
-        // Break dependency cycle
-        if (daoContext==null) {
-            daoContext= beanFactory.getBean(DaoContext.class);
-        }
-        if (mapperContext==null) {
-            mapperContext = beanFactory.getBean(MapperContext.class);
-        }
-        if (objectMapper==null){
-            objectMapper = beanFactory.getBean(ObjectMapper.class);
-        }
 
-        final LocalDateTime transactionDateTime =LocalDateTime.now();
+        final LocalDateTime transactionDateTime = LocalDateTime.now();
 
-        transactionRevisions.entrySet().forEach(objectRevisionEntry -> {
+        transactionRevisions.forEach((key, value) -> {
             // No dao operations can be made before flush
             // Set logged in user
-            objectRevisionEntry.getValue().setUser(getAuthenticatedUser());
+            value.setUser(getAuthenticatedUser());
             // Set type of entity
-            objectRevisionEntry.getValue().setEntityType(
-                    EntityType.fromClass(objectRevisionEntry.getValue().getClass()));
+            value.setEntityType(
+                    EntityType.fromClass(value.getClass()));
             // Set current time
-            objectRevisionEntry.getValue().setLogDate(transactionDateTime);
+            value.setLogDate(transactionDateTime);
 
-            getPayloadAndId(objectRevisionEntry.getKey(),objectRevisionEntry.getValue()); // Id cannot be set before flush
-            daoContext.getRevisionDao().save(objectRevisionEntry.getValue()); // Create new revision
+            getPayloadAndId(key, value); // Id cannot be set before flush
+            daoContext.getRevisionDao().save(value); // Create new revision
         });
         transactionRevisions.clear(); // Clean used map, as the same interceptor is used while running spring
     }
 
-
-    // Cycle in the loading of hibernate/ spring beans, this method solves it
-    @Override
-    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
-        this.beanFactory = beanFactory;
-
+    /**
+     * This bean depends on other beans. For that reason we have to wait for the application to be completely
+     * initialised. Spring will then call this method for us.
+     *
+     * We can't autowire this, as this causes Spring to crash because there are cyclic dependencies.
+     *
+     * @param event The event containing the new context.
+     */
+    @EventListener
+    public void handleContextRefresh(ContextRefreshedEvent event) {
+        BeanFactory factory = event.getApplicationContext().getAutowireCapableBeanFactory();
+        daoContext = factory.getBean(DaoContext.class);
+        mapperContext = factory.getBean(MapperContext.class);
+        objectMapper = factory.getBean(ObjectMapper.class);
     }
 }
