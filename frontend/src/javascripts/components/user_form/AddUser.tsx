c8c89fc385aeb0338a052e804d029cf7ead6863b
nnmak <nils.mak@gmail.com>
2017-05-11 21:25:38 +0200

    error quickfix

--- frontend/src/javascripts/components/user_form/AddUser.tsx
+++ frontend/src/javascripts/components/user_form/AddUser.tsx
@@ -33,11 +33,8 @@ class AddUser extends React.Component<{}, State> {
 
   public onSubmit(e: any): void {
     e.preventDefault();
-    let success = (data : any) => redirect_to(`/users/${data.id}`);
-    let setErrors = (e : Form.Error[]) => this.setState({ errors: e });
     const setErrors = (es: Form.Error[]) => this.setState({ errors: es });
     const success = (data: any) => redirect_to(`/users/${data.id}`);
-
     postUser(this.state.user, success, Errors.handle(setErrors));
   }
 
