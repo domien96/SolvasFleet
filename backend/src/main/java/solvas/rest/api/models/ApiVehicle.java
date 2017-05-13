06f63c68d012f8729bb8b03b7a99a3eb6650bbd7
Niko Strijbol <strijbol.niko@gmail.com>
2017-05-13 16:03:20 +0200

    Add unique validation for vin

--- backend/src/main/java/solvas/rest/api/models/ApiVehicle.java
+++ backend/src/main/java/solvas/rest/api/models/ApiVehicle.java
@@ -1,7 +1,7 @@
 package solvas.rest.api.models;
 
 import org.hibernate.validator.constraints.NotBlank;
-import org.joda.time.DateTime;
+import solvas.rest.utils.validators.UniqueVin;
 import solvas.service.models.validators.AfterLocalDateTime;
 import solvas.service.models.validators.Vin;
 
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 /**
  * Vehicle in the API layer
  */
+@UniqueVin
 public class ApiVehicle extends ApiModel {
 
     private String licensePlate;
