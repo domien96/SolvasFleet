e8b4ba0731848d335bf9973e7b9b219bddafe9c5
David Vandorpe <david.vandorpe@ugent.be>
2017-05-14 02:07:52 +0200

    Small test refactor

--- backend/src/itest/java/solvas/authorization/AuthorizationTest.java
+++ backend/src/itest/java/solvas/authorization/AuthorizationTest.java
@@ -1,4 +1,4 @@
-package solvas;
+package solvas.authorization;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -17,9 +17,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import solvas.Application;
 import solvas.authentication.WebSecurityConfig;
+import solvas.authentication.ajax.LoginRequest;
 import solvas.authentication.jwt.JwtSettings;
 import solvas.authorization.MethodSecurityConfig;
 import solvas.persistence.api.dao.TestConfig;
@@ -65,19 +68,15 @@ public class AuthorizationTest {
 
     @Test
     public void testUserAccessForAccount() throws Exception {
-        String token = getToken("metus.vitae.velit@facilisismagnatellus.net", "WBL90DHP2RU");
-        mockMvc.perform(get("/companies").header("X-Authorization", "Bearer " + token))
+        mockMvc.perform(authenticate(get("/companies"), UserFixtures.ADMINISTRATOR))
                 .andExpect(status().isOk());
     }
 
-    private String getToken(String username, String password) throws Exception {
+    private MockHttpServletRequestBuilder authenticate(MockHttpServletRequestBuilder builder, LoginRequest user) throws Exception {
         ObjectMapper mapper=new ObjectMapper();
         mapper.findAndRegisterModules();
-        ObjectNode o = mapper.createObjectNode();
-        o.put("username", username);
-        o.put("password", password);
-        String response = mockMvc.perform(post("/auth/login").contentType(MediaType.APPLICATION_JSON_UTF8).content(o.toString())).andReturn().getResponse().getContentAsString();
-        System.out.println(response);
-        return JsonPath.read(response, "accessToken.token");
+        String response = mockMvc.perform(post("/auth/login").contentType(MediaType.APPLICATION_JSON_UTF8).content(mapper.writeValueAsString(user))).andReturn().getResponse().getContentAsString();
+        String token = JsonPath.read(response, "accessToken.token");
+        return builder.header("X-Authorization", "Bearer " + token);
     }
 }
---
+++ backend/src/itest/java/solvas/authorization/UserFixtures.java
@@ -0,0 +1,7 @@
+package solvas.authorization;
+
+import solvas.authentication.ajax.LoginRequest;
+
+public class UserFixtures {
+    public final static LoginRequest ADMINISTRATOR = new LoginRequest("metus.vitae.velit@facilisismagnatellus.net", "WBL90DHP2RU");
+}
