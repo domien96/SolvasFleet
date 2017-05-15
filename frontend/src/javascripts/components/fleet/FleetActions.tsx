715aca43ddb02491e17ab3b42e81bfe546295949
klivdnbu <karel.vandenbussche@ugent.be>
2017-05-15 13:52:35 +0200

    archive vehicle from fleet with confirm

--- frontend/src/javascripts/components/fleet/FleetActions.tsx
+++ frontend/src/javascripts/components/fleet/FleetActions.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card   from '../app/Card.tsx';
+import Confirm from 'react-confirm-bootstrap';
 import T from 'i18n-react';
 
 interface Props {
@@ -18,9 +19,15 @@ const FleetActions : React.StatelessComponent<Props> = props => {
     <div className='card-content fleet-actions'>
     <h3><label>{ T.translate('fleet.actions') }</label></h3>
     <div className='actions pull-right'>
-    <button className={'btn btn-danger'+disableButton} onClick={props.callToArchive}>
-      <span className='glyphicon glyphicon-plus' /> Archive
-    </button>
+      <Confirm
+        onConfirm={props.callToArchive}
+        body="Are you sure you want to archive these vehicle(s) ?"
+        confirmText="Confirm Archive"
+        title="Archive fleet">
+        <button className={'btn btn-danger'+disableButton} onClick={props.callToArchive}>
+          <span className='glyphicon glyphicon-plus' /> Archive
+        </button>
+      </Confirm>
     </div></div></Card>
   );
 
