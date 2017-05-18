3f49557eed5f4b1423951a11e69413064e497caa
sjabasti <Steven.Bastiaens@UGent.be>
2017-05-18 16:23:41 +0200

    fixed audit nullpointer

--- backend/src/main/java/solvas/service/audit/AuditInterceptor.java
+++ backend/src/main/java/solvas/service/audit/AuditInterceptor.java
@@ -53,6 +53,10 @@ public class AuditInterceptor extends EmptyInterceptor {
             return false;
         }
 
+        if (entity instanceof InvoiceItem) {
+            return false;
+        }
+
         //make revision
         Revision revision = new Revision();
         revision.setMethod(MethodType.UPDATE);
@@ -67,6 +71,10 @@ public class AuditInterceptor extends EmptyInterceptor {
     public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
         // Make sure there is no infinite loop
         if (entity instanceof Revision) {
+            return false;
+        }
+
+        if (entity instanceof InvoiceItem) {
             return false;
         }
 
@@ -90,6 +98,9 @@ public class AuditInterceptor extends EmptyInterceptor {
     public void onDelete(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) {
         // Make sure there is no infinite loop
         if (entity instanceof Revision) {
+            return;
+        }
+        if (entity instanceof InvoiceItem) {
             return;
         }
 
