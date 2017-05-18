ddc21e9060b4911154da4d4846f8c567f100ac57
David Vandorpe <david.vandorpe@ugent.be>
2017-05-18 14:49:19 +0200

    leaving rest to Niko

--- backend/src/main/java/solvas/persistence/api/dao/CompanyDao.java
+++ backend/src/main/java/solvas/persistence/api/dao/CompanyDao.java
@@ -1,6 +1,9 @@
 package solvas.persistence.api.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+import solvas.persistence.api.Filter;
 import solvas.service.models.Company;
 import solvas.persistence.api.Dao;
 
@@ -23,6 +26,8 @@ public interface CompanyDao extends Dao<Company> {
      * @return The companies.
      */
     Collection<Company> findByName(String name);
+
+    Page<Company> findByIdIn(Pageable pagination, Collection<Integer> ids);
 
     /**
      * Find the company with a certain vat number.
--- backend/src/main/java/solvas/rest/controller/CompanyRestController.java
+++ backend/src/main/java/solvas/rest/controller/CompanyRestController.java
@@ -2,16 +2,24 @@ package solvas.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import solvas.authorization.ApiPermissionStrings;
+import solvas.authorization.CompanyExtractor;
+import solvas.rest.utils.PagedResult;
 import solvas.service.models.Company;
 import solvas.rest.api.models.ApiCompany;
 import solvas.rest.query.CompanyFilter;
 import solvas.service.CompanyService;
+import solvas.rest.utils.PagedResult;
+
 
 import javax.validation.Valid;
+import java.util.Collection;
 
 
 /**
@@ -21,14 +29,18 @@ import javax.validation.Valid;
 @RestController
 public class CompanyRestController extends AbstractRestController<Company,ApiCompany> {
 
+    private final CompanyExtractor companyExtractor;
+    private final CompanyService companyService;
     /**
      * Rest controller for Company
      *
      * @param service service class for companies
      */
     @Autowired
-    public CompanyRestController(CompanyService service) {
+    public CompanyRestController(CompanyService service, CompanyExtractor companyExtractor) {
         super(service);
+        this.companyService = service;
+        this.companyExtractor = companyExtractor;
     }
 
 
@@ -44,7 +56,20 @@ public class CompanyRestController extends AbstractRestController<Company,ApiCom
      */
     @RequestMapping(value = "/companies", method = RequestMethod.GET)
     public ResponseEntity<?> listAll(Pageable pagination, CompanyFilter filter, BindingResult result) {
-        return super.listAll(pagination, filter, result);
+        // If there are errors in the filtering, send bad request.
+        if (result.hasErrors()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Collection<Integer> companyIds = companyExtractor.extractCompanyIds(SecurityContextHolder.getContext().getAuthentication());
+        PagedResult<ApiCompany> pagedResult;
+        if(companyIds == null) {
+            pagedResult = new PagedResult<>(service.findAll(pagination, filter));
+        } else {
+            pagedResult = new PagedResult<>(service.findAll(pagination, filter, companyIds));
+        }
+
+        return new ResponseEntity<>(pagedResult, HttpStatus.OK);
     }
 
     @Override
