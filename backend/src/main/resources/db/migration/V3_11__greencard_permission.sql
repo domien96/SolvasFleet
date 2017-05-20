a17d64db8be75d597259d083dbd0c0a9177ffb37
Domien Van Steendam <domien.vansteendam@ugent.be>
2017-05-20 18:35:06 +0200

    added global scope

--- backend/src/main/resources/db/migration/V3_11__greencard_permission.sql
+++ backend/src/main/resources/db/migration/V3_11__greencard_permission.sql
@@ -1,1 +1,8 @@
-INSERT INTO permissions (scope) VALUES ('read:company:greencard');
+INSERT INTO permissions (scope) VALUES ('read:company:greencard'); -- permission for a specific company
+INSERT INTO permissions (scope) VALUES ('read:companies:greencard'); -- global permission
+
+-- Give the admin this new permission
+INSERT INTO role_permissions (role_id, permission_id) VALUES((SELECT role_id FROM roles where function = 'administrator'),
+                                                             (SELECT permission_id FROM permissions WHERE scope ='read:company:greencard'));
+INSERT INTO role_permissions (role_id, permission_id) VALUES((SELECT role_id FROM roles where function = 'administrator'),
+                                                             (SELECT permission_id FROM permissions WHERE scope ='read:companies:greencard'));
