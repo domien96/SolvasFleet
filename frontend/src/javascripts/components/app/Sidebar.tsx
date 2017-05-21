1684a65add71ec7b7d628d6e054588a75a76601b
nnmak <nils.mak@gmail.com>
2017-05-21 09:42:02 +0200

    changed signedinstatus from email from sub to get a user from the sub id and then find the email

--- frontend/src/javascripts/components/app/Sidebar.tsx
+++ frontend/src/javascripts/components/app/Sidebar.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router';
 import classNames from 'classnames';
 import { slide as Menu } from 'react-burger-menu';
 import LanguageSwitcher from './LanguageSwitcher.tsx';
+import { fetchUser } from '../../actions/user_actions.ts';
 import T from 'i18n-react';
 
 import Auth from '../../modules/Auth.ts';
@@ -33,45 +34,69 @@ interface Props {
   updateLanguage: () => void;
 }
 
-const Sidebar: React.StatelessComponent<Props> = props => {
-  const info = (
-    <div>
-      <div id='logo'>
-        <Link to='/'>
-          <h2>SolvasFleet</h2>
-        </Link>
-      </div>
-      <ul className='nav'>
-        <SidebarLink path='/users'>{ T.translate('user.users') }</SidebarLink>
-        <SidebarLink path='/clients'>{ T.translate('company.clients') }</SidebarLink>
-        <SidebarLink path='/vehicles'>{ T.translate('vehicle.vehicles') }</SidebarLink>
-        <SidebarLink path='/log'>{ T.translate('log.log') }</SidebarLink>
-        <SidebarLink path='/auth'>{ T.translate('auth.permissionSettings') }</SidebarLink>
-      </ul>
-      <ul className='nav session-actions'>
-        <li className='plain'>
-          { T.translate('app.signedInStatus') } <p>{ Auth.getLocalSub() }</p>
-        </li>
-        <li>
-          <Link to='/sign_out'>
-            { T.translate('app.signOut') }
-          </Link>
-        </li>
-      </ul>
-      <LanguageSwitcher updateLanguage={ props.updateLanguage } />
-    </div>
-  );
+interface State {
+  email: string;
+}
 
-  return (
-    <div>
-      <Menu className='mobile-menu'>
-        {info}
-      </Menu>
-      <nav className='standard-menu'>
-        {info}
-      </nav>
-    </div>
-  );
-};
+
+
+class Sidebar extends React.Component<Props, State> {
+
+  constructor() {
+    super();
+    this.state = { email: '' };
+  }
+
+  componentDidMount() {
+    this.fetchUserEmail(parseInt(Auth.getLocalSub(),10));
+  }
+
+  fetchUserEmail(id: number) {
+    fetchUser(id, ((data: any) => {
+      this.setState({ email: data.email });
+    }));
+  }
+
+  render() {
+    const info = (
+      <div>
+        <div id='logo'>
+          <Link to='/'>
+            <h2>SolvasFleet</h2>
+          </Link>
+        </div>
+        <ul className='nav'>
+          <SidebarLink path='/users'>{ T.translate('user.users') }</SidebarLink>
+          <SidebarLink path='/clients'>{ T.translate('company.clients') }</SidebarLink>
+          <SidebarLink path='/vehicles'>{ T.translate('vehicle.vehicles') }</SidebarLink>
+          <SidebarLink path='/log'>{ T.translate('log.log') }</SidebarLink>
+          <SidebarLink path='/auth'>{ T.translate('auth.permissionSettings') }</SidebarLink>
+        </ul>
+        <ul className='nav session-actions'>
+          <li className='plain'>
+            { T.translate('app.signedInStatus') } <p>{ this.state.email }</p>
+          </li>
+          <li>
+            <Link to='/sign_out'>
+              { T.translate('app.signOut') }
+            </Link>
+          </li>
+        </ul>
+        <LanguageSwitcher updateLanguage={ this.props.updateLanguage } />
+      </div>
+    );
+
+    return (
+      <div>
+        <Menu className='mobile-menu'>
+          { info }
+        </Menu>
+        <nav className='standard-menu'>
+          { info }
+        </nav>
+      </div>
+    );
+  }
+}
 
 export default Sidebar;
